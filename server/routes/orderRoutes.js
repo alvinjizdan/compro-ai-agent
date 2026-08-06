d04fb6c5daf4cb888eecdf4cb6436e365a324542
Alvin <jizdan.a14@gmail.com>
2026-08-06 19:16:46 +0700

    fix(auth): protect order management routes

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
 
-router.get('/', orderController.getOrders);
-router.post('/', orderController.createOrder);
-router.put('/:id', orderController.updateOrderStatus);
-router.delete('/:id', orderController.deleteOrder);
+const { verifyToken, requireAdmin } = require('../middleware/authMiddleware');
+
+router.get('/', verifyToken, requireAdmin, orderController.getOrders);
+router.post('/', orderController.createOrder); // Public for customer checkout
+router.put('/:id', verifyToken, requireAdmin, orderController.updateOrderStatus);
+router.delete('/:id', verifyToken, requireAdmin, orderController.deleteOrder);
 
 module.exports = router;
